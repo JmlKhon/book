52ec3812d181697e476ece07dcd032baf854549f
Jamolkhon Umarov(Jamal) <conjuma@partner.waters.com>
2022-10-21 17:42:51 +0300

    get method

---
+++ book/Data/Services/BookService.cs
@@ -0,0 +1,37 @@
+﻿using book.Data.Models;
+
+namespace book.Data.Services
+{
+    public class BookService
+    {
+        private AppDbContext _context;
+
+        public BookService(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public void AddBook(BookVM.BookVM book)
+        {
+            var _book = new Book()
+            {
+                Title = book.Title,
+                Description = book.Description,
+                isRead = book.isRead,
+                Rate = 1,
+                Genre = book.Genre,
+                DateRead = book.DateRead,
+                CoverUrl = book.CoverUrl,
+            };
+
+            _context.Books.Add(_book);
+            _context.SaveChanges();
+        }
+
+        public List<Book> GetAllBooks() => _context.Books.ToList();
+
+        public Book GetBookById(int bookId) => _context.Books.FirstOrDefault(n => n.Id == bookId);
+
+
+    }
+}
--- book/Program.cs
+++ book/Program.cs
@@ -1,4 +1,5 @@
 using book.Data;
+using book.Data.Services;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -9,6 +10,7 @@ builder.Services.AddDbContext<AppDbContext>(o =>
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddTransient<BookService>();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
